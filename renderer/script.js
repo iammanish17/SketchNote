35d413da7955972394e7fd75362e1003d711f85f
Manish Kundu <manishkundu@live.com>
2021-10-18 22:15:40 +0530

    Added quill js.

--- renderer/script.js
+++ renderer/script.js
@@ -4,14 +4,47 @@ var fs = require('fs');
 var drawingSection = document.getElementById("drawingSection");
 var canvas = document.getElementById("drawingCanvas");
 var ctx = canvas.getContext('2d');
-ctx.strokeStyle = "red";
-ctx.strokeRect(100, 100, 200, 500);
 var newNoteButton = document.getElementById("create-note-button");
+
+var Delta = Quill.import('delta');
+var quill = new Quill('#editor', {
+  modules: {
+    toolbar: [
+      [{ header: [1, 2, 3, false] }],
+      ['bold', 'italic', 'underline'],
+      ['code-block']
+    ]
+  },
+  placeholder: 'Write something here...',
+  theme: 'snow'
+});
+const BlockPrototype = Quill.import("blots/block");
+
+class CustomBlock extends BlockPrototype {
+  constructor(domNode, value) {
+    super(domNode, value);
+    this.format("size", "18px");
+  }
+
+  static tagName = "P";
+
+  format(name, value) {
+    if (name === "size") {
+      this.domNode.style.fontSize = value;
+    } else {
+      super.format(name, value);
+    }
+  }
+}
+
+Quill.register(CustomBlock, true);
 
 window.onload = async () => {
   };
 
   newNoteButton.onclick = () => {
+      console.log(quill.getContents());
+      return;
       var noteID = new Date().getTime().toString();
       db.set(noteID, {
           title: "new note",
